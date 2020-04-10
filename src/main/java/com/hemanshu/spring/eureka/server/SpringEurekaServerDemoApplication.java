b8d66eafbbcd072a5609ee8e487bee33e3ea9417
Hemanshu Patira <hemanshupatira@gmail.com>
2020-04-10 16:07:32 +0530

    Initial commit

---
+++ src/main/java/com/hemanshu/spring/eureka/server/SpringEurekaServerDemoApplication.java
@@ -0,0 +1,15 @@
+package com.hemanshu.spring.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringEurekaServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaServerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hemanshu/spring/eureka/server/SpringEurekaServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemanshu.spring.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
